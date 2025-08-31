c4c833fdadd1bca53aad5da4f2a6ef8257b90aec
mdh00 <madhini00@gmail.com>
2025-08-31 23:26:11 +0530

    dummy text added

--- components/data.ts
+++ components/data.ts
@@ -41,7 +41,7 @@ export const projects = [
 
 export const user = {
   name: "Emy Coligado",
-  email: "madhini00@gmail.com",
+  email: "yourname@example.com",
   linkedinUrl: "https://linkedin.com/in/emycoligado",
   linkedinUsername: "emycoligado",
   bio:
